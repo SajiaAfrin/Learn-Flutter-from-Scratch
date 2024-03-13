a47bf0a8b93998797d207dbde2123c4714d3bc23
Sajia Afrin <sajiaafrinmitul05@gmail.com>
2024-03-13 21:14:41 +0600

    list_gridView-builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learnfromscratch/listview_gridview.dart';
 import 'package:learnfromscratch/row_colum-wrap.dart';
 import 'package:learnfromscratch/textbutton_elevatedbutton.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const ButtonWidget(),
+      home: const ListGrid(),
     );
   }
 }
